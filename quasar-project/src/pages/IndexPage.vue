7a4e79f41a1205083becd52d921467f9526e0a40
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-26 01:25:52 +0000

    🐣Boton de reiniciar

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,22 +1,21 @@
 <template>
   <q-page class="flex flex-center text-whith">
-    <div class="row full-width items-center"> 
-    <div class="col text-center">
-      <q-btn 
-      round 
-      icon="remove" 
-      size="xl"/>
+    <div class="row full-width items-center">
+      <div class="col text-center">
+        <q-btn round icon="remove" size="xl" />
+      </div>
+      <div class="col text-center text-h2">
+        23
+      </div>
+      <div class="col text-center">
+        <q-btn round icon="add" size="xl"
+        />
+      </div>
+      <div class="row">
+        <q-btn round icon="restart_alt" size="xl"
+        />
+      </div>
     </div>
-    <div class="col text-center text-h2">
-      23
-    </div>
-    <div class="col text-center">
-      <q-btn 
-      round 
-      icon="add" 
-      size="xl"/>
-    </div>
-  </div>
   </q-page>
 </template>
 
