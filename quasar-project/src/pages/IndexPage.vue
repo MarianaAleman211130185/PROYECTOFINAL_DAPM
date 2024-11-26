b1075ecb41b568eaafc88b89251555c75e68e4ec
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-26 01:22:25 +0000

    🐣Botones Se agregaron botones a la intefaz de add y remove

--- quasar-project/src/pages/IndexPage.vue
+++ quasar-project/src/pages/IndexPage.vue
@@ -1,9 +1,21 @@
 <template>
-  <q-page class="flex flex-center">
-    <div class="row full-width"> 
-    <div class="col text-center"></div>
-    <div class="col text-center"></div>
-    <div class="col text-center"></div>
+  <q-page class="flex flex-center text-whith">
+    <div class="row full-width items-center"> 
+    <div class="col text-center">
+      <q-btn 
+      round 
+      icon="remove" 
+      size="xl"/>
+    </div>
+    <div class="col text-center text-h2">
+      23
+    </div>
+    <div class="col text-center">
+      <q-btn 
+      round 
+      icon="add" 
+      size="xl"/>
+    </div>
   </div>
   </q-page>
 </template>
