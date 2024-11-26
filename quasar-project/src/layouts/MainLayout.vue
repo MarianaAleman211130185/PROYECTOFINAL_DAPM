dfde1e9feb7f0d7333146e60088d92d412cd8cec
MarianaAleman211130185 <99774985+MarianaAleman211130185@users.noreply.github.com>
2024-11-26 01:01:15 +0000

    🐣Background Se le cambio le color y la imagen al Background

--- quasar-project/src/layouts/MainLayout.vue
+++ quasar-project/src/layouts/MainLayout.vue
@@ -1,5 +1,8 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" class="bg-primary">
+  <q-img
+      src="Background.jpg"
+      class="fit absolute"></q-img>
     <q-page-container>
       <router-view />
     </q-page-container>
